75efdf234bfb159a1003a74624e0feda063e628a
Bezimienii <noreply>
2018-04-14 19:16:08 +0000

    Add component mycom.html

---
+++ ui/mycom.html.reel/mycom.html.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/mycom.html.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Mycom.html
+ * @extends Component
+ */
+exports.Mycom.html = Component.specialize(/** @lends Mycom.html.prototype */{
+
+});
